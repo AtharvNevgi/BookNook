d46ea5678e46c023bdc17778665ed51b5b4dbdde
Atharva Nevgi <atharva.nevgi579@gmail.com>
2025-10-03 00:46:59 +0530

    added admin login logic

--- backend/controllers/adminAuthController.js
+++ backend/controllers/adminAuthController.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-
+const User = require("../models/userModel")
 
 const getAdminlogin = (req, res) => {
     res.render("admin/adminlogin");
 }
 
-module.exports = {getAdminlogin};
+const postAdminlogin = async (req, res) => {
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const user = await User.findOne({email:email});
+
+        if(!user){
+            res.send("No Such User Found");
+        }
+        if(user.role !== "admin"){
+            console.log(user.role)
+            res.send("Not a admin login please login through user login or register");
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(isMatch){
+            res.cookie("userId", user.id.toString(), {
+                maxAge: 1000 * 60 * 600,
+            })
+            res.send("Hello Admin");
+        }
+        else{
+            res.send("Invalid Login credentials");
+        }
+    }
+    catch(err){
+        res.status(404).send("Invalid Email", err);
+        console.log(err)
+    }
+}
+
+module.exports = {getAdminlogin, postAdminlogin};
--- backend/routes/adminAuthRouter.js
+++ backend/routes/adminAuthRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const {getAdminlogin} = require("../controllers/adminAuthController");
+const {getAdminlogin, postAdminlogin} = require("../controllers/adminAuthController");
 const adminRouter = express.Router();
 
 adminRouter.get("/", getAdminlogin);
+
+adminRouter.post("/adminlogin", postAdminlogin)
 
 module.exports = {adminRouter};
