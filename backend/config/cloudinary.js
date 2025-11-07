edf273dae846711fec1b0df64caed301c2eabf94
AtharvNevgi <atharva.nevgi579@gmail.com>
2025-11-07 12:06:01 +0530

    successfully added cloudinary image upload on new book add

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,7 +1,11 @@
 const cloudinary = require("cloudinary").v2;
+require("dotenv").config({quiet:true});
+
 const CLOUD_NAME = process.env.CLOUD_NAME;
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
+
+// console.log(CLOUD_NAME, API_KEY, API_SECRET);
 
 cloudinary.config({
     cloud_name : CLOUD_NAME,
